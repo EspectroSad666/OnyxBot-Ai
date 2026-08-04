c307c25759d2de8dd2fd63a11953869e006739e9
EspectroSad666 <octaviomedero71@gmail.com>
2026-08-04 04:35:08 -0500

    Connect receiver with WhatsApp startup
    Linked incoming WhatsApp messages to the connection startup process.

--- src/whatsapp/connection.js
+++ src/whatsapp/connection.js
@@ -1,5 +1,6 @@
 const { default: makeWASocket } = require("@whiskeysockets/baileys");
 const handleConnectionUpdate = require("./events");
+const startReceiver = require("./receiver");
 
 async function connectWhatsApp() {
   console.log("📱 Preparando conexión con WhatsApp...");
@@ -9,6 +10,8 @@ async function connectWhatsApp() {
   });
 
   sock.ev.on("connection.update", handleConnectionUpdate);
+
+  startReceiver(sock);
 
   return sock;
 }
