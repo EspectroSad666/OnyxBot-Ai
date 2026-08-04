cb0675d965d9263d5ad4d26a3a4a45457eac365b
EspectroSad666 <octaviomedero71@gmail.com>
2026-08-04 05:11:14 -0500

    Create personalized welcome system
    Added personalized greetings using member names.

---
+++ src/plugins/personalWelcome.js
@@ -0,0 +1,12 @@
+function personalWelcome(memberName) {
+  return `
+⚫ ¡Bienvenido ${memberName}! 👋
+
+Soy OnyxBot 🤖
+Espero que disfrutes la comunidad.
+
+Recuerda respetar las reglas del grupo 📜
+`;
+}
+
+module.exports = personalWelcome;
